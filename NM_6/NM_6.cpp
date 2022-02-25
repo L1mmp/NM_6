08ddb717e0410c6b5cf8bc2326570d99e69fc245
Dmitriy <46874245+L1mmp@users.noreply.github.com>
2022-02-25 10:38:07 +0300

    Fixed version

--- NM_6/NM_6.cpp
+++ NM_6/NM_6.cpp
@@ -2,9 +2,12 @@
 
 int main()
 {
-	QrEq qreq(1.4, 0.8, pow(10, -6));
-	qreq.SetXY(4, -1);
-	qreq.SetXY(6, -1.5);
+	setlocale(LC_ALL, "russian");
+	cout << "Первое приближение: " << endl;
+	QrEq qreq(2.7, 1.4, 1.e-6);
+
+	cout << endl << "Второе приближение: " << endl;
+	qreq.SetXY(1.0, 0.4);
 	return 0;
 }
 
