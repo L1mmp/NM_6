7054fe63d4aed93ff3a3bfa0a0f30e74460c05eb
Dmitriy <46874245+L1mmp@users.noreply.github.com>
2022-02-25 10:37:45 +0300

    Fixed version

--- NM_6/QrEq.cpp
+++ NM_6/QrEq.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-double QrEq::F(double x, double y)
+double QrEq::F(double x, double y)// Превая функция системы.
 {
 	return pow(2, x - y) - y - 1;
 }
-double QrEq::G(double x, double y)
+double QrEq::G(double x, double y)// Вторая функция системы.
 {
-	return pow(y, 3) + pow(x, 2) - 6 * x + 6;
+	return y*y*y + x*x - 6 * x + 6;
 }
 QrEq::QrEq(double x0, double y0, double E)
 {
@@ -23,36 +23,36 @@ QrEq::~QrEq()
 {
 	ofile.close();
 }
-double QrEq::F_X(double x, double y)
+double QrEq::F_X(double x, double y)// Производная F по x.
 {
-	return pow(2, x - y) * log10(2);
+	return pow(2, x - y) * log(2);
 }
-double QrEq::F_Y(double x, double y)
+double QrEq::F_Y(double x, double y)// Производная F по y.
 {
-	return  pow(2, x - y) * log10(2) - 1;
+	return  -pow(2, x - y) * log(2) - 1;
 }
-double QrEq::G_X(double x, double y)
+double QrEq::G_X(double x, double y)// Производная G по x.
 {
 	return 2 * x - 6;
 }
-double QrEq::G_Y(double x, double y)
+double QrEq::G_Y(double x, double y)// Производная G по y.
 {
-	return 3 * pow(y, 3) - 2 * y;
+	return 3 * y * y;
 }
-void QrEq::SetXY(double x0, double y0)
+void QrEq::SetXY(double x0, double y0)// Задает начальное приближение.
 {
 	x = x0;
 	y = y0;
 	StartIter();
 }
-void QrEq::FillW()
+void QrEq::FillW()// Составляет матрицу W.
 {
 	W[0][0] = F_X(x, y);
 	W[0][1] = F_Y(x, y);
 	W[1][0] = G_X(x, y);
 	W[1][1] = G_Y(x, y);
 }
-void QrEq::StartIter()
+void QrEq::StartIter()// Итерационный процесс.
 {
 
 	double bufX = 0;
@@ -67,14 +67,15 @@ void QrEq::StartIter()
 		bufY = y;
 		x = bufX - W_1[0][0] * F(bufX, bufY) - W_1[0][1] * G(bufX, bufY);
 		y = bufY - W_1[1][0] * F(bufX, bufY) - W_1[1][1] * G(bufX, bufY);
-	} while (Stop(bufX, bufY) == false);
+		OutResult();
+	} while (!Stop(bufX, bufY));
 	OutResult();
 }
-double QrEq::det()
+double QrEq::det()// Считает детерминант.
 {
 	return W[0][0] * W[1][1] - 1 * W[1][0] * W[0][1];
 }
-void QrEq::FillW_1()
+void QrEq::FillW_1()// Гененрирует обратную матрицу.
 {
 	double Det = det();
 	if (Det == 0)
@@ -87,16 +88,20 @@ void QrEq::FillW_1()
 	W_1[1][0] = -1 * W[1][0] / Det;
 	W_1[1][1] = W[0][0] / Det;
 }
-bool QrEq::Stop(double bufX, double bufY)
+bool QrEq::Stop(double bufX, double bufY)// Признак остановки итерационного процесса.
 {
-	if (iter > 199)return true;
-	if (fabs(x - bufX) > e)return false;
-	if (fabs(y - bufY) > e)return false;
+	if (iter >= 20) return true;
+	if ((fabs(x - bufX) > e) || (fabs(y - bufY) > e)) return false;
 	return true;
 }
-void QrEq::OutResult()
+
+void QrEq::OutResult()// Выводит результат на консоль и файл.
 {
+	cout << "x = " << setprecision(6) << x << endl;
+	cout << "y = " << setprecision(6) << y << endl;
+	cout << "Iter = " << iter << endl << endl;
+
 	ofile << "x = " << setprecision(6) << x << endl;
 	ofile << "y = " << setprecision(6) << y << endl;
-	ofile << "Iter = " << iter << endl;
+	ofile << "Iter = " << iter << endl << endl;
 }
